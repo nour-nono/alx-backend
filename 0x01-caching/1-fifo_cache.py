2cda6bdf9bc7fc4bf00359e848adff470930944a
nour el deen <115364839+nour-nono@users.noreply.github.com>
2024-07-31 22:07:45 +0300

    Update 1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """First-In First-Out caching module.
 """
-# from collections import OrderedDict
-from collections import deque
+from collections import OrderedDict
+# from collections import deque
 from base_caching import BaseCaching
 
 
@@ -15,35 +15,35 @@ class FIFOCache(BaseCaching):
         """Initializes the cache.
         """
         super().__init__()
-        # self.cache_data = OrderedDict()
-        self .ls = deque()
+        self.cache_data = OrderedDict()
+        # self .ls = deque()
 
-    # def put(self, key, item):
-    #     """Adds an item in the cache.
-    #     """
-    #     if key and item:
-    #         self.cache_data[key] = item
-    #     if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-    #         first_key, _ = self.cache_data.popitem(False)
-    #         print("DISCARD:", first_key)
-
-    # def get(self, key):
-    #     """Retrieves an item by key.
-    #     """
-    #     return self.cache_data.get(key, None)
     def put(self, key, item):
-        """ Add an item in the cache
+        """Adds an item in the cache.
         """
-        if not (key and item):
-            return
-        if self.MAX_ITEMS == len(self.cache_data):
-            x = self.ls.popleft()
-            self.cache_data.pop(x, 0)
-            print("DISCARD:", x)
-        self.ls.append(key)
-        self.cache_data[key] = item
+        if key and item:
+            self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key, _ = self.cache_data.popitem(False)
+            print("DISCARD:", first_key)
 
     def get(self, key):
-        """ Get an item by key
+        """Retrieves an item by key.
         """
         return self.cache_data.get(key, None)
+    # def put(self, key, item):
+    #     """ Add an item in the cache
+    #     """
+    #     if not (key and item):
+    #         return
+    #     if self.MAX_ITEMS == len(self.cache_data):
+    #         x = self.ls.popleft()
+    #         self.cache_data.pop(x, 0)
+    #         print("DISCARD:", x)
+    #     self.ls.append(key)
+    #     self.cache_data[key] = item
+
+    # def get(self, key):
+    #     """ Get an item by key
+    #     """
+    #     return self.cache_data.get(key, None)
