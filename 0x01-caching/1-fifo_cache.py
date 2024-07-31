b783221a5d71f5bccad8bc2640cc8679ab0fa001
nour el deen <115364839+nour-nono@users.noreply.github.com>
2024-07-31 22:01:30 +0300

    Update 1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -17,16 +17,32 @@ class FIFOCache(BaseCaching):
         super().__init__()
         self.cache_data = OrderedDict()
 
+    # def put(self, key, item):
+    #     """Adds an item in the cache.
+    #     """
+    #     if key and item:
+    #         self.cache_data[key] = item
+    #     if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+    #         first_key, _ = self.cache_data.popitem(False)
+    #         print("DISCARD:", first_key)
+
+    # def get(self, key):
+    #     """Retrieves an item by key.
+    #     """
+    #     return self.cache_data.get(key, None)
     def put(self, key, item):
-        """Adds an item in the cache.
+        """ Add an item in the cache
         """
-        if key and item:
-            self.cache_data[key] = item
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            first_key, _ = self.cache_data.popitem(False)
-            print("DISCARD:", first_key)
+        if not (key and item):
+            return
+        if self.MAX_ITEMS == len(self.cache_data):
+            x = self.ls.popleft()
+            self.cache_data.pop(x, 0)
+            print("DISCARD:", x)
+        self.ls.append(key)
+        self.cache_data[key] = item
 
     def get(self, key):
-        """Retrieves an item by key.
+        """ Get an item by key
         """
         return self.cache_data.get(key, None)
