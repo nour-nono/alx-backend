a9bb4a48617d6fab8d55d106f7ffc226b8f7b7a2
nour el deen <115364839+nour-nono@users.noreply.github.com>
2024-07-31 23:22:43 +0300

    Update 2-lifo_cache.py

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Last-In First-Out caching module.
 """
-# from collections import OrderedDict
-from collections import deque
+from collections import OrderedDict
+# from collections import deque
 
 from base_caching import BaseCaching
 
@@ -16,38 +16,38 @@ class LIFOCache(BaseCaching):
         """Initializes the cache.
         """
         super().__init__()
-        # self.cache_data = OrderedDict()
-        self .ls = deque()
+        self.cache_data = OrderedDict()
+        # self .ls = deque()
 
-    # def put(self, key, item):
-    #     """Adds an item in the cache.
-    #     """
-    #     if key is None or item is None:
-    #         return
-    #     if key not in self.cache_data:
-    #         if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-    #             last_key, _ = self.cache_data.popitem(True)
-    #             print("DISCARD:", last_key)
-    #     self.cache_data[key] = item
-    #     self.cache_data.move_to_end(key, last=True)
-
-    # def get(self, key):
-    #     """Retrieves an item by key.
-    #     """
-    #     return self.cache_data.get(key, None)
     def put(self, key, item):
-        """ Add an item in the cache
+        """Adds an item in the cache.
         """
-        if not (key and item):
+        if key is None or item is None:
             return
-        if self.MAX_ITEMS == len(self.cache_data):
-            x = self.ls.pop()
-            self.cache_data.pop(x, 0)
-            print("DISCARD:", x)
-        self.ls.append(key)
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                last_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", last_key)
         self.cache_data[key] = item
+        self.cache_data.move_to_end(key, last=True)
 
     def get(self, key):
-        """ Get an item by key
+        """Retrieves an item by key.
         """
         return self.cache_data.get(key, None)
+    # def put(self, key, item):
+    #     """ Add an item in the cache
+    #     """
+    #     if not (key and item):
+    #         return
+    #     if self.MAX_ITEMS == len(self.cache_data):
+    #         x = self.ls.pop()
+    #         self.cache_data.pop(x, 0)
+    #         print("DISCARD:", x)
+    #     self.ls.append(key)
+    #     self.cache_data[key] = item
+
+    # def get(self, key):
+    #     """ Get an item by key
+    #     """
+    #     return self.cache_data.get(key, None)
