24ec323d060a4844a375e63a4414a72d5f5b588e
nour el deen <115364839+nour-nono@users.noreply.github.com>
2024-08-01 03:00:58 +0300

    Update 4-mru_cache.py

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -17,23 +17,41 @@ class MRUCache(BaseCaching):
         super().__init__()
         self.cache_data = OrderedDict()
 
+    # def put(self, key, item):
+    #     """Adds an item in the cache.
+    #     """
+    #     if key is None or item is None:
+    #         return
+    #     if key not in self.cache_data:
+    #         if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+    #             mru_key, _ = self.cache_data.popitem(False)
+    #             print("DISCARD:", mru_key)
+    #         self.cache_data[key] = item
+    #         self.cache_data.move_to_end(key, last=False)
+    #     else:
+    #         self.cache_data[key] = item
+
+    # def get(self, key):
+    #     """Retrieves an item by key.
+    #     """
+    #     if key is not None and key in self.cache_data:
+    #         self.cache_data.move_to_end(key, last=False)
+    #     return self.cache_data.get(key, None)
     def put(self, key, item):
-        """Adds an item in the cache.
+        """ Add an item in the cache
         """
-        if key is None or item is None:
+        if not (key and item):
             return
         if key not in self.cache_data:
-            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-                mru_key, _ = self.cache_data.popitem(False)
-                print("DISCARD:", mru_key)
-            self.cache_data[key] = item
-            self.cache_data.move_to_end(key, last=False)
-        else:
-            self.cache_data[key] = item
+            if self.MAX_ITEMS == len(self.cache_data):
+                print("DISCARD:", self.cache_data.popitem(last=True)[0])
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key, last=True)
 
     def get(self, key):
-        """Retrieves an item by key.
+        """ Get an item by key
         """
-        if key is not None and key in self.cache_data:
-            self.cache_data.move_to_end(key, last=False)
-        return self.cache_data.get(key, None)
+        x = self.cache_data.get(key, None)
+        if x:
+            self.cache_data.move_to_end(key, last=True)
+        return x
